e711b0a80eb1fcbcf59ba1dce059a4041206aee4
Neil Shevlin <neilshevlin.98@gmail.com>
2022-10-24 13:55:46 +0100

    Added input

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import * as React from 'react'
+import Box from '@mui/material/Box';
+import { Button, TextField} from '@mui/material';
 
 export default function Home() {
   return (
@@ -14,6 +17,37 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to our Sweng calculator application
         </h1>
+        <Box
+          component="form"
+          sx={{
+            width: 500,
+            mt: 10,
+            maxWidth: '100%',
+            '& .MuiOutlinedInput-root': {
+              '& fieldset': {
+                borderColor: 'white',
+              },
+              '&:hover fieldset': {
+                borderColor: 'white',
+              },
+              '&.Mui-focused fieldset': {
+                borderColor: 'white',
+              },
+            },
+            '& .MuiInputBase-input': {
+              color: 'white',
+            },
+            '& .MuiInputLabel-root': {
+              color: 'white',
+            },
+
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField fullWidth label="Enter some problem for calculation" id="fullWidth"/>
+      </Box>
+         
       </main>
 
       <footer className={styles.footer}>
