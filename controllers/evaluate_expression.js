c63589fe378e239bee3556a7872a8b17aa22ebec
bairdr <bairdr@tcd.ie>
2022-10-29 16:24:48 +0100

    check last char

--- controllers/evaluate_expression.js
+++ controllers/evaluate_expression.js
@@ -16,3 +16,172 @@ function checkFirstChar(expression) {
     }
     return true;
 }
+
+function checkLastChar(expression) {
+    if(isNaN(parseInt(expression[expression.length-1]))) {
+        return false;
+    }
+    return true;
+}
+
+function checkForInvalidCharacters(expression) {
+    for(let i = 0; i < expression.length; i++) {
+        if(isNaN(parseInt(expression[i])) && expression[i] != "+" && expression[i] != "-" && expression[i] != "*" && expression[i] != "/") {
+            return false;// invalid character
+        }
+    }
+    return true;
+}
+
+function checkForOperators(expression) {
+    for(let i = 0; i < expression.length; i++) {
+        if(isNaN(parseInt(expression[i])) && expression[i] != "-") {
+            return true;
+        }
+    }
+    return false;
+}
+
+function checkForInvalidOperators(expression) {
+    for(let i = 0; i < expression.length; i++) {
+        if(expression[i] == "/" && expression[i+1] == "0") {
+            return false; // we can't divide by 0
+        }
+    }
+    return true;
+}
+
+function checkForMultipleOperators(expression) {
+    for(let i = 0; i < expression.length; i++) {
+        if(isNaN(parseInt(expression[i])) && expression[i] != "-" && expression[i] != ".") {
+            if(isNaN(parseInt(expression[i+1])) && expression[i+1] != "-" && expression[i+1] != ".") {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+
+
+function checkForNumbers(expression) {
+    for(let i = 0; i < expression.length; i++) {
+        if(!isNaN(parseInt(expression[i]))) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function checkForParenthesis(expression) {
+    for(let i = 0; i < expression.length; i++) {
+        if(expression[i] == "(" || expression[i] == ")") {
+            return true;
+        }
+    }
+    return false;
+}
+
+function checkForInvalidParenthesis(expression) {
+    let open = 0;
+    let close = 0;
+    for(let i = 0; i < expression.length; i++) {
+        if(expression[i] == "(") {
+            open++;
+        }
+        else if(expression[i] == ")") {
+            close++;
+        }
+    }
+    if(open != close) {
+        return true;
+    }
+    return false;
+}
+
+function checkForMultipleParenthesis(expression) {
+    for(let i = 0; i < expression.length; i++) {
+        if(expression[i] == "(" || expression[i] == ")") {
+            if(expression[i+1] == "(" || expression[i+1] == ")") {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+
+function checkForDecimal(expression) {
+    for(let i = 0; i < expression.length; i++) {
+        if(expression[i] == ".") {
+            return true;
+        }
+    }
+    return false;
+}
+
+function checkForInvalidDecimals(expression) {
+    for(let i = 0; i < expression.length; i++) {
+        if(expression[i] == "." && isNaN(parseInt(expression[i+1]))) {
+            return false; // we can't have a decimal without a number after it
+        }
+    }
+    return true;
+}
+
+function checkForMultipleDecimals(expression) {
+    for(let i = 0; i < expression.length; i++) {
+        if(expression[i] == ".") {
+            if(expression[i+1] == ".") {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function checkForNegative(expression) {
+    for(let i = 0; i < expression.length; i++) {
+        if(expression[i] == "-") {
+            return true;
+        }
+    }
+    return false;
+}
+
+function checkForInvalidNegative(expression) {
+    for(let i = 0; i < expression.length; i++) {
+        if(expression[i] == "-" && isNaN(parseInt(expression[i+1]))) {
+            return false; // we can't have a negative without a number after it
+        }
+    }
+    return true;
+}
+
+
+function checkForLetters(expression) {
+    for(let i = 0; i < expression.length; i++) {
+        if(isNaN(parseInt(expression[i])) && expression[i] != "-" && expression[i] != "." && expression[i] != "(" && expression[i] != ")") {
+            return true;
+        }
+    }
+    return false;
+}
+
+function checkForMultipleNegative(expression) {
+    for(let i = 0; i < expression.length; i++) {
+        if(expression[i] == "-") {
+            if(expression[i+1] == "-") {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+
+
+
+
+
+
