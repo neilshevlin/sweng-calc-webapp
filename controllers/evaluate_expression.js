018b175771c7694843439779e5b4fe1ad1810783
bairdr <bairdr@tcd.ie>
2022-10-29 16:51:17 +0100

    check for multiple  parenthesis

--- controllers/evaluate_expression.js
+++ controllers/evaluate_expression.js
@@ -97,6 +97,16 @@ function checkForInvalidParenthesis(expression) {
     return false;
 }
 
+function checkForMultipleParenthesis(expression) {
+    for(let i = 0; i < expression.length; i++) {
+        if(expression[i] == "(" || expression[i] == ")") {
+            if(expression[i+1] == "(" || expression[i+1] == ")") {
+                return true;
+            }
+        }
+    }
+    return false;
+}
 
 
 
