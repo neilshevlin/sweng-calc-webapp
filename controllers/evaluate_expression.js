6a7766ace3e1fcfee5ed244c5753b7699bb03ff0
bairdr <bairdr@tcd.ie>
2022-10-29 16:49:18 +0100

    check for parenthesis

--- controllers/evaluate_expression.js
+++ controllers/evaluate_expression.js
@@ -71,7 +71,14 @@ function checkForNumbers(expression) {
     return false;
 }
 
-
+function checkForParenthesis(expression) {
+    for(let i = 0; i < expression.length; i++) {
+        if(expression[i] == "(" || expression[i] == ")") {
+            return true;
+        }
+    }
+    return false;
+}
 
 
 
