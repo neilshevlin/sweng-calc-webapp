246cbc8b7ac01984b12c83749164bce6282e1e37
Neil Shevlin <neilshevlin.98@gmail.com>
2022-11-03 16:55:35 +0000

    Updated and fixed tests

--- controllers/validate_expression.js
+++ controllers/validate_expression.js
@@ -18,14 +18,14 @@
 
          if(expr == "")
          {
-            return 1;
+            return {code: 1, message: "Operator issue or empty expression"};
          }
          else if(expr.length == 1)
          {
              if(isNumber(expr.charAt(0)))
-                 return 0;
+                 return true;
              else
-                 return 1;
+                 return {code: 1, message: "Operator issue or empty expression"};
          }
          for(let i = 0; i < expr.length; i++)
          {
@@ -37,10 +37,10 @@
 
                      //If it isn't proper operator placement
                      if(!canBeOperator)
-                         return 1;
+                         return {code: 1, message: "Operator issue or empty expression", valid: false};
                          //Final charatcer cannot be operator
                      else if(i == expr.length-1)
-                         return 1;
+                        return {code: 1, message: "Operator issue or empty expression", valid: false};
                          //Otherwise, set canBeOperator to false (prevent double operators)
                      else
                          canBeOperator = false;
@@ -55,7 +55,7 @@
                          if(i == 0)
                          {
                              if(!isOperator(expr.charAt(i+1)))
-                                 return 3;
+                                return {code: 3, message: "Leading 0", valid: false};
                          }
                          //Check if expr isn't +/-/*0... (invalid)
                          else if(isOperator(expr.charAt(i-1)))
@@ -65,7 +65,7 @@
                              {
                                  //If expr isn't operating on 0 (e.g. +0+)
                                  if(!isOperator(expr.charAt(i+1)))
-                                     return 3;
+                                     return {code: 3, message: "Leading 0", valid: false};
                              }
                          }
                      }
@@ -77,16 +77,16 @@
                     decimalCount++;
                     if(decimalCount >1)
                     {
-                        return 4;
+                        return {code: 4, message: "Incorrect decimal placings", valid: false};
                     }
                     // if it is a . followed by an operator, this is an error. If 
                     if(isOperator(expr.charAt(i+1)))
                     {
-                        return 4;
+                        return {code: 4, message: "Incorrect decimal placings", valid: false};
                     }
                     else if(!isNumber(expr.charAt(i+1)))
                     {
-                        return 4;
+                        return {code: 4, message: "Incorrect decimal placings", valid: false};
                     }
                  }
                  else if(currChar == '(')
@@ -94,12 +94,12 @@
                     //if 2 consecutive left brackets
                     if(leftBracketUsed)
                     {
-                        return 5;
+                        return {code: 5, message: "Incorrect use of brackets", valid: false};
                     }
                     //number before the bracket is an error: 2+3(4*5)
                     else if(isNumber(expr.charAt(i-1)))
                     {
-                        return 5;
+                        return {code: 5, message: "Incorrect use of brackets", valid: false};
                     }
                     leftBracketUsed = true;
                     
@@ -109,7 +109,7 @@
                     //if no left bracket was used before it
                     if(!leftBracketUsed)
                     {
-                        return 5;
+                        return {code: 5, message: "Incorrect use of brackets", valid: false};
                     }
                     //finished checking log function
                     if(checkingLog)
@@ -125,7 +125,7 @@
                         //if the next character is not an operator this is an error: (5+3)5
                         if(!isOperator(expr.charAt(i+1)))
                         {
-                            return 5;
+                            return {code: 5, message: "Incorrect use of brackets", valid: false};
                         }
                     }
                     
@@ -137,14 +137,14 @@
                     checkingLog = true;
                     if(expr.charAt(i+1)!='o' || expr.charAt(i+2) != 'g' || expr.charAt(i+3) != '(')
                     {
-                        return 6;
+                        return {code: 6, message: "Incorrect log function", valid: false};
                     }
                     else if(i!=0)
                     {
                         //if it is not an operator before the log function it is an error: 34log(7+4)
                         if(!isOperator(expr.charAt(i-1)))
                         {
-                            return 6;
+                            return {code: 6, message: "Incorrect log function", valid: false};
                         }
                     }
                     // var correctLog = checkLogFunction(expr, i);
@@ -153,14 +153,14 @@
                  {
                     if(!checkingLog)
                     {
-                        return 6;
+                        return {code: 6, message: "Incorrect log function", valid: false};
                     }
                  }
                  else if(currChar =='g')
                  {
                     if(!checkingLog)
                     {
-                        return 6;
+                        return {code: 6, message: "Incorrect log function", valid: false};
                     }
                  }
                  //checking exp
@@ -169,14 +169,14 @@
                     checkingExp = true;
                     if(expr.charAt(i+1)!='x' || expr.charAt(i+2) != 'p' || expr.charAt(i+3) != '(')
                     {
-                        return 6;
+                        return {code: 6, message: "Incorrect log function", valid: false};
                     }
                     else if(i!=0)
                     {
                         //if it is not an operator before the log function it is an error: 34log(7+4)
                         if(!isOperator(expr.charAt(i-1)))
                         {
-                            return 6;
+                            return {code: 6, message: "Incorrect log function", valid: false};
                         }
                     }
                     // var correctLog = checkLogFunction(expr, i);
@@ -185,26 +185,26 @@
                  {
                     if(!checkingExp)
                     {
-                        return 6;
+                        return {code: 6, message: "Incorrect log function", valid: false};
                     }
                  }
                  else if(currChar =='p')
                  {
                     if(!checkingExp)
                     {
-                        return 6;
+                        return {code: 6, message: "Incorrect log function", valid: false};
                     }
                  }
                      
                  //If it isn't a valid character
                  else
-                     return 2;
+                     return {code: 2, message: "Invalid character", valid: false};
          }
          //Check to see if last character ISN'T operator
          if(isOperator(expr.charAt(expr.length-1)))
-         return 1;
+         return {code: 1, message: "Operator issue or empty expression", valid: false};
      //Else, return 0 (is valid expression)
-     return 0;
+     return {code: 0, message: "Valid expression", valid: true};
      }
      
      function isOperator(currChar)
--- tests/index.test.js
+++ tests/index.test.js
@@ -17,164 +17,161 @@ describe("Parsing tests", () => {
 // 8. isValidexpr for the ^ power operator
 // 9. isValidexpr for the natural log operator
 // 10. isValidexpr for exponent functions
+ 
+  test('test simple expression exp(2)', () => {//failing
+    var result = evaluateExpression("exp(2)");
+    expect(result).toBe("7.38905609893065");
+  })
+  
+  test('test simple expression log(2)', () => {//failing
+    var result = evaluateExpression("log(2)");
+    expect(result).toBe("0.6931471805599453");
+  })
+  test('test simple expression 1-5', () => {
+    var result = evaluateExpression("1-5");//failing
+    expect(result).toBe("-4");
+  })
+  test('test simple expression 1+1', () => {
+    var result = isValidexpr("1+1");
+    expect(result.code).toBe(0);
+  })
+  test('test simple expression 1-2', () => {
+    var result = isValidexpr("1-2");
+    expect(result.code).toBe(0);
+  })
 
-  test('test empty expression', () => {
-    var result = isValidexpr("");
-    expect(result).toBe(1);
+  test('test simple expression 1*2', () => {
+    var result = isValidexpr("1*2");
+    expect(result.code).toBe(0);
+  })
+
+  test('test simple expression 1/2', () => {
+    var result = isValidexpr("1/2");
+    expect(result.code).toBe(0);
+  })
+
+  test('test simple expression 1+2+3', () => {
+    var result = isValidexpr("1+1+1");
+    expect(result.code).toBe(0);
   })
   test('test simple expression 30^2', () => {
     var result = evaluateExpression("30^2");
     expect(result).toBe("900");
   })
-  test('test simple expression exp2', () => {
-    var result = evaluateExpression("exp2");
-    expect(result).toBe("7.38905609893065");
-  })
-  test('test simple expression ln2', () => {
-    var result = evaluateExpression("ln2");
-    expect(result).toBe("0.6931471805599453");
-  })
-  test('test simple expression log2', () => {
-    var result = evaluateExpression("log2");
-    expect(result).toBe("0.6931471805599453");
-  })
-  test('test simple expression 1-5', () => {
-    var result = evaluateExpression("1-5");
-    expect(result).toBe("-4");
-  })
-  test('test simple expression 1+2', () => {
-    var result = isValidexpr("1+1");
-    expect(result).toBe(0);
-  })
-  test('test simple expression 1-2', () => {
-    var result = isValidexpr("1-1");
-    expect(result).toBe(0);
-  })
-
-  test('test simple expression 1*2', () => {
-    var result = isValidexpr("1*1");
-    expect(result).toBe(0);
-  })
-
-  test('test simple expression 1/2', () => {
-    var result = isValidexpr("1/2");
-    expect(result).toBe(0);
-  })
-
-  test('test simple expression 1+2+3', () => {
-    var result = isValidexpr("1+1+1");
-    expect(result).toBe(0);
+  test('test empty expression', () => {
+    var result = isValidexpr("");
+    expect(result.code).toBe(1);
   })
 
   test('test simple expression 1+2-3', () => {
     var result = isValidexpr("1+1-1");
-    expect(result).toBe(0);
+    expect(result.code).toBe(0);
   })
 
   test('test simple expression 1+2*3', () => {
     var result = isValidexpr("1+1*1");
-    expect(result).toBe(0);
+    expect(result.code).toBe(0);
   })
 
   test('test simple expression 1+2/3', () => {
     var result = isValidexpr("1+1/1");
-    expect(result).toBe(0);
+    expect(result.code).toBe(0);
   })
   test('test expression 10*30.0', () => {
     var result = isValidexpr("10*30.0");
-    expect(result).toBe(0);
+    expect(result.code).toBe(0);
   })
   test('test expression 10*30.2', () => {
     var result = isValidexpr("10*30.2");
-    expect(result).toBe(0);
+    expect(result.code).toBe(0);
   })
 test('test expression 10*30.2^45', () => {
   var result = isValidexpr("10*30.2^45");
-  expect(result).toBe(0);
+  expect(result.code).toBe(0);
 })
 test('test expression 2+(3+2)', () => {
   var result = isValidexpr("2+(3+2)");
-  expect(result).toBe(0);
+  expect(result.code).toBe(0);
 })
 test('test expression 2+(3+2)*3+(4.2)', () => {
   var result = isValidexpr("2+(3+2)*3+(4.2)");
-  expect(result).toBe(0);
+  expect(result.code).toBe(0);
 })
 test('test expression 2+log(7)', () => {
   var result = isValidexpr("2+log(7)");
-  expect(result).toBe(0);
+  expect(result.code).toBe(0);
 })
 test('test expression log(8+4*7)*5', () => {
   var result = isValidexpr("log(8+4*7)*5");
-  expect(result).toBe(0);
+  expect(result.code).toBe(0);
 })
 test('test expression 2+exp(7)', () => {
   var result = isValidexpr("2+exp(7)");
-  expect(result).toBe(0);
+  expect(result.code).toBe(0);
 })
 test('test expression exp(8+4*7)*5', () => {
   var result = isValidexpr("exp(8+4*7)*5");
-  expect(result).toBe(0);
+  expect(result.code).toBe(0);
 })
 test('test expression 1++2*3', () => {
   var result = isValidexpr("1++2*3");
-  expect(result).toBe(1);
+  expect(result.code).toBe(1);
 })
 test('test expression 1+2*c3', () => {
   var result = isValidexpr("1+2*c3");
-  expect(result).toBe(2);
+  expect(result.code).toBe(2);
 })
 test('test expression 1+2*03', () => {
   var result = isValidexpr("1+2*03");
-  expect(result).toBe(3);
+  expect(result.code).toBe(3);
 })
 test('test expression 10*30.+', () => {
   var result = isValidexpr("10*30.+");
-  expect(result).toBe(4);
+  expect(result.code).toBe(4);
 })
 test('test expression 10*30.204.56', () => {
   var result = isValidexpr("10*30.204.56");
-  expect(result).toBe(4);
+  expect(result.code).toBe(4);
 })
 test('test expression 10.4*3(3+2)', () => {
   var result = isValidexpr("10.4*3(3+2)");
-  expect(result).toBe(5);
+  expect(result.code).toBe(5);
 })
 test('test expression 10.4*3(3+2', () => {
   var result = isValidexpr("10.4*3(3+2)");
-  expect(result).toBe(5);
+  expect(result.code).toBe(5);
 })
 test('test expression 10.4*33+2)', () => {
   var result = isValidexpr("10.4*3(3+2)");
-  expect(result).toBe(5);
+  expect(result.code).toBe(5);
 })
 test('test expression 10.4*3((3+2)', () => {
   var result = isValidexpr("10.4*3(3+2)");
-  expect(result).toBe(5);
+  expect(result.code).toBe(5);
 })
 test('test expression log(8+(4*7)*5', () => {
   var result = isValidexpr("log(8+(4*7)*5");
-  expect(result).toBe(5);
+  expect(result.code).toBe(5);
 })
 test('test expression logg(8+4*7)*5', () => {
   var result = isValidexpr("logg(8+4*7)*5");
-  expect(result).toBe(6);
+  expect(result.code).toBe(6);
 })
 test('test expression 5+3log(2)', () => {
   var result = isValidexpr("5+3log(2)");
-  expect(result).toBe(6);
+  expect(result.code).toBe(6);
 })
 test('test expression exp(8+(4*7)*5', () => {
   var result = isValidexpr("exp(8+(4*7)*5");
-  expect(result).toBe(5);
+  expect(result.code).toBe(5);
 })
 test('test expression expp(8+4*7)*5', () => {
   var result = isValidexpr("expp(8+4*7)*5");
-  expect(result).toBe(6);
+  expect(result.code).toBe(6);
 })
 test('test expression 5+3exp(2)', () => {
   var result = isValidexpr("5+3exp(2)");
-  expect(result).toBe(6);
+  expect(result.code).toBe(6);
 })
   test('test simple expression 1*2', () => {
     var result = evaluateExpression("1*2");
@@ -223,10 +220,6 @@ test('test expression 5+3exp(2)', () => {
   test('test simple expression 2^2^2', () => {
     var result = evaluateExpression("2^2^2");
     expect(result).toBe("16");
-  })
-  test('test simple expression log(3^2 + ln(8^exp(2))/3.333)', () => {
-    var result = evaluateExpression("log(3^2 + ln(8^exp(2))/3.333)");
-    expect(result).toBe("2.610804380306326");
   })
   test('test simple expression 2.4 + exp(2)', () => {
     var result = evaluateExpression("2.4 + exp(2)");
