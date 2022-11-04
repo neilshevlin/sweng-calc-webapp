39cc59a33aaa5691101414e78eb813cc5cfe5ef5
Indigo Bosworth <indigobosworth@gmail.com>
2022-11-04 15:18:29 +0000

    added testing of trignometry functions

--- tests/index.test.js
+++ tests/index.test.js
@@ -238,4 +238,8 @@ test('test expression 5+3exp(2)', () => {
     var result = evaluateExpression("4/0");
     expect(result).toBe("Error: Division by Zero");
   })
+  test('test simple expression sin(6) + log(20) - cos(1) * tan(4/5)', () => {
+    var result = evaluateExpression("sin(6) + log(20) - cos(1) * tan(4/5)");
+    expect(result).toBe("2.160000688770009");
+  })
 });
